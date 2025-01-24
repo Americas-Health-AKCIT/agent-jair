92a25aef51295a56eb56aa9ffc0eb229aaac6709
arantesvictorl <victorlsarantes@gmail.com>
2025-01-24 15:00:09 -0300

    refactor: atualiza prompts de justificativa para melhorar clareza e estrutura

--- justificador/justificador.py
+++ justificador/justificador.py
@@ -2,7 +2,7 @@ from langchain_openai import ChatOpenAI
 import pandas as pd
 
 from justificador.src.rag import retrival_item
-from justificador.src.prompts import PROMPT_RECUSADO, PROMPT_AUTORIZADO
+from justificador.src.prompts import JUSTIFICATIVA_PROMPT
 
 def justificador(id_item, item_info, paciente_info, status=False):
 
@@ -19,9 +19,9 @@ def justificador(id_item, item_info, paciente_info, status=False):
 
 
     if status:
-        formatted_prompt = PROMPT_AUTORIZADO.format(DS_CLASSIFICACAO_1=item_info['DS_CLASSIFICACAO_1'], DS_ITEM=item_info['DS_ITEM'], DOCS_RELEVANTES=docs, DADOS_PACIENTE=paciente_info)
+        formatted_prompt = JUSTIFICATIVA_PROMPT.format(DS_CLASSIFICACAO_1=item_info['DS_CLASSIFICACAO_1'], DS_ITEM=item_info['DS_ITEM'], DOCS_RELEVANTES=docs, DADOS_PACIENTE=paciente_info, DECISAO="autorizado")
     else:
-        formatted_prompt = PROMPT_RECUSADO.format(DS_CLASSIFICACAO_1=item_info['DS_CLASSIFICACAO_1'], DS_ITEM=item_info['DS_ITEM'], DOCS_RELEVANTES=docs, DADOS_PACIENTE=paciente_info)
+        formatted_prompt = JUSTIFICATIVA_PROMPT.format(DS_CLASSIFICACAO_1=item_info['DS_CLASSIFICACAO_1'], DS_ITEM=item_info['DS_ITEM'], DOCS_RELEVANTES=docs, DADOS_PACIENTE=paciente_info, DECISAO="recusado")
     response = llm.invoke(formatted_prompt)
 
     return response.content, fontes
